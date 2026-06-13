06a3b501243b1632faeadf6c22598780979e863b
Rick-Astley2 <191059137+Rick-Astley2@users.noreply.github.com>
2026-06-12 21:20:24 -0500

    Finished the first two my_each and my_Map all tests passing

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,10 @@
 module Enumerable
-  # Your code goes here
+  def my_map
+    return self.to_enum unless block_given?
+    mapped_array = []
+    self.my_each { |element| mapped_array << yield(element)}
+    return mapped_array
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +12,11 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    return self.to_enum unless block_given?
+    for element in self
+      yield(element)
+    end
+  end
 end
+
